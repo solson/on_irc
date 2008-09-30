9302354a749336ddcec76238a33e25d76701590c
Scott Olson <scott@scott-olson.org>
2008-09-29 22:12:45 -0600

    added lots of redundant self's to commands.rb

--- lib/irc/commands.rb
+++ lib/irc/commands.rb
@@ -9,24 +9,24 @@ class IRC
 	
 	# Sends a message to the +recipient+, which may be a user or a channel.
 	def privmsg(recipient, message)
-		raw "PRIVMSG #{recipient} :#{message}"
+		self.raw "PRIVMSG #{recipient} :#{message}"
 	end
 	
 	alias msg privmsg
 	
 	# Sends a notice to the +recipient+, which may be a user or a channel.
 	def notice(recipient, message)
-		raw "NOTICE #{recipient} :#{message}"
+		self.raw "NOTICE #{recipient} :#{message}"
 	end
 	
 	# Sends an action message.
 	def action(recipient, message)
-		ctcp_request(recipient, "ACTION", message)
+		self.ctcp_request(recipient, "ACTION", message)
 	end
 	
 	# Sends an action notice. (Note that this is not supported by many IRC clients.)
 	def action_notice(recipient, message)
-		ctcp_reply(recipient, "ACTION", message)
+		self.ctcp_reply(recipient, "ACTION", message)
 	end
 	
 	## CTCP
@@ -35,47 +35,47 @@ class IRC
 	def ctcp_request(recipient, ctcp, param=nil)
 		request = ctcp
 		request << ' ' + param if param
-		privmsg(recipient, "\1#{request}\1")
+		self.privmsg(recipient, "\1#{request}\1")
 	end
 	
 	# Sends a CTCP reply.
 	def ctcp_reply(recipient, ctcp, param=nil)
 		reply = ctcp
 		reply << ' ' + param if param
-		notice(recipient, "\1#{reply}\1")
+		self.notice(recipient, "\1#{reply}\1")
 	end
 	
 	
 	# Join the specified +channel+.
 	def join(channel)
-		raw "JOIN #{channel}"
+		self.raw "JOIN #{channel}"
 	end
 	
 	# Leave the specified +channel+, with +reason+ if it is given.
 	def part(channel, reason=nil)
 		if reason
-			raw "PART #{channel} :#{reason}"
+			self.raw "PART #{channel} :#{reason}"
 		else
-			raw "PART #{channel}"
+			self.raw "PART #{channel}"
 		end
 	end
 	
 	
 	
 	def ping(message)
-		raw "PING :#(message)"
+		self.raw "PING :#(message)"
 	end
 	
 	def pong(message)
-		raw "PONG :#{message}"
+		self.raw "PONG :#{message}"
 	end
 
 
   def quit(message=nil)
     if message
-      raw "QUIT :#{message}"
+      self.raw "QUIT :#{message}"
     else
-      raw "QUIT"
+      self.raw "QUIT"
     end
   end
 
