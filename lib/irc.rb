2e07a07777b4902e41f5aed44f8711b32bfc028b
Scott Olson <scott@scott-olson.org>
2009-01-02 16:38:07 -0600

    Moved IRC::Config to it's own file.

--- lib/irc.rb
+++ lib/irc.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.dirname(__FILE__)
 require 'socket'
 require 'irc/configdslhelper'
+require 'irc/config'
 require 'irc/commands'
 require 'irc/mask'
 require 'irc/user'
@@ -10,32 +11,6 @@ require 'irc/events'
 require 'irc/server'
 
 class IRC
-  class Config
-    attr_accessor :nick, :ident, :realname, :servers
-    
-    def self.new_from_dsl(dsl)
-      conf = new
-      conf.nick = dsl.nick
-      conf.ident = dsl.ident || dsl.nick
-      conf.realname = dsl.realname || dsl.nick
-      conf.servers = dsl.servers || []
-      conf
-    end
-    
-    class DSL
-      dsl_accessor :nick, :ident, :realname
-      
-      def server(name, &blk)
-        @servers ||= []
-        @servers << Server.new(name, &blk)
-      end
-      
-      def servers
-        @servers
-      end
-    end
-  end
-  
   attr_accessor :servers, :config
   
   def initialize(&blk)
