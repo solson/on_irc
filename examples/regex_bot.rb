fcd2623a1a29f3e4eb0eba687fa2c3387b4317c0
Scott Olson <scott@scott-olson.org>
2009-09-14 23:30:35 -0600

    fixed the regex bot; !!!s/// stuff was broken

--- examples/regex_bot.rb
+++ examples/regex_bot.rb
@@ -8,24 +8,14 @@ class Backlog
 
   def initialize(length)
     @array = []
-    self.length = length
-  end
-
-  def length=(length)
-    raise 'negative length' if length < 0
-
     @length = length
-
-    if @array.length > @length
-      @array.pop until @array.length == @length
-    elsif @array.length < @length
-      @array << nil until @array.length == @length
-    end
   end
 
   def <<(msg)
     @array.unshift msg
     @array.pop if @array.length > @length
+
+    self
   end
 
   def [](index)
@@ -48,8 +38,7 @@ class Backlog
 end
 
 MAX_BANGS = 3
-#TEST_STRING = 'The quick brown fox jumped over the lazy dog.'
-CH_USER_MEMORY = Hash.new(Hash.new(Backlog.new(MAX_BANGS))) # Hash of Hashes of Backlogs {channel => {user => [history, of, messages]}}
+CH_USER_MEMORY = {}
 CHANNEL_MEMORY = {}
 
 IRC.configure do
@@ -69,13 +58,12 @@ end
 IRC[:eighthbit].on :'001' do |e|
   IRC.send(e.server, :join, '#programming')
   IRC.send(e.server, :join, '#offtopic')
-#  IRC.send(e.server, :privmsg, '#programming', "test string: #{TEST_STRING}")
 end
 
 IRC.on :privmsg do |e|
-  next unless e.params[0][0,1] == '#' # only works in channels
+  next unless e.params[0][0,1] == '#' # make sure regex replace only happens in channels
   channel = e.params[0]
-  user = e.prefix
+  user = e.prefix.split('!').first
   message = e.params[1]
 
   if e.params[1] =~ %r"^(!{0,#{MAX_BANGS}})s/((?:[^\\/]|\\.)*)/((?:[^\\/]|\\.)*)/(?:(\w*))?"
@@ -104,7 +92,6 @@ IRC.on :privmsg do |e|
       answer = target.sub(match, replace)
     end
 
-#    IRC.send(e.server, :privmsg, e.params[0], "bangs: #{bangs.size} | match: #{match.inspect} | replace: #{replace.inspect} | flags: #{flags.inspect}")
     if bangs.length > 0 || CHANNEL_MEMORY[channel][0] == e.prefix.split('!').first
       IRC.send(e.server, :privmsg, e.params[0], e.prefix.split('!').first + ' meant: ' + answer)
     else
@@ -112,8 +99,10 @@ IRC.on :privmsg do |e|
     end
 
   else
+    CH_USER_MEMORY[channel] ||= {}
+    CH_USER_MEMORY[channel][user] ||= Backlog.new(MAX_BANGS)
     CH_USER_MEMORY[channel][user] << message
-    CHANNEL_MEMORY[channel] = [e.prefix.split('!').first, message]
+    CHANNEL_MEMORY[channel] = [user, message]
   end
 end
 
